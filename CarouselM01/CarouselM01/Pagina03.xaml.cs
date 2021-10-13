38bac7705d51c1c907494a0fa4f46f87cbd64202
hssuper <higorsouza2306@gmail.com>
2021-10-13 11:34:36 -0300

    Adicionar arquivos de projeto.

---
+++ CarouselM01/CarouselM01/Pagina03.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselM01
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Pagina03 : ContentPage
+    {
+        public Pagina03()
+        {
+            InitializeComponent();
+        }
+
+        private void Bt2VoltarMainpage(object sender, EventArgs e)
+        {
+            Navigation.PopToRootAsync();
+        }
+    }
+}
